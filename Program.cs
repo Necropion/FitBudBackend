d977aeed4fbcf3b0157c61e42da71bfc7a7a8bfb
unknown <jokas3@gmail.com>
2026-05-02 17:30:21 +0200

    feat: connect db, add initial migrations

--- Program.cs
+++ Program.cs
@@ -1,8 +1,44 @@
+using Npgsql;
+using Microsoft.EntityFrameworkCore;
+using FitBudBackend.data;
+using Backend.services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(connString));
+
+// Dependency Injections
+builder.Services.AddScoped<UserService>();
+
+try
+{
+
+    using var conn = new NpgsqlConnection(connString);
+    conn.Open();
+
+    Console.WriteLine("Connected to the database successfully");
+
+    conn.Close();
+} catch (Exception ex)
+{
+    Console.WriteLine("Failed to connect");
+    Console.WriteLine(ex.Message);
+}
+
 var app = builder.Build();
+
+// Run Database Migrations
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    
+    Console.WriteLine("Applying migrations...");
+    db.Database.Migrate();
+    Console.WriteLine("Migrations applied.");
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- controllers/UserController.cs
+++ controllers/UserController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Backend.models;
+using Backend.services;
 
 namespace Backend.controllers;
 
@@ -7,16 +8,33 @@ namespace Backend.controllers;
 [Route("api/user")]
 public class UserController : ControllerBase
 {
+    private readonly UserService _userService;
+
+    public UserController(UserService userService)
+    {
+        _userService = userService;
+    }
+
     // Get User List
     [HttpGet]
-    public ActionResult<User> GetUsers()
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
     {
-        var user = new User
-        {
-            Id = 1,
-            Name = "Bob"
-        };
+        var users = await _userService.FetchAllUsers();
 
-        return Ok(user);
+        return Ok(users);
+    }
+
+    // Post Single User
+    [HttpPost]
+    public async Task<ActionResult<User>> PostUser(User user)
+    {
+        var newUser = await _userService.CreateUser(user);
+
+        if (newUser == null)
+        {
+            return BadRequest("Failed to create user");
+        }
+
+        return Ok(newUser);
     }
 }
